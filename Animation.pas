ef1b694d60e7868c3118e666997c744c3e3d6ab6
Albertbuh <91022025+Albertbuh@users.noreply.github.com>
2022-05-11 22:22:05 +0300

    Update Animation.pas
    a

--- Animation.pas
+++ Animation.pas
@@ -1,254 +1,281 @@
-unit multik;
+unit Animation;
 
 interface
-
+{HEllo git}
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls,
-  Animation, BackgroundObjects;
+  Vcl.Graphics, System.Types;
+// Vcl.Graphics -> TCanvas
+// System.Types -> TPoint
+
+// https://docwiki.embarcadero.com/Libraries/Sydney/en/System.Types.TPoint
 
 type
-  TForm1 = class(TForm)
-    Timer1: TTimer;
-    procedure Timer1Timer(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
-    procedure FormKeyPress(Sender: TObject; var Key: Char);
-    procedure FormPaint(Sender: TObject);
-  private
-    { Private declarations }
-  public
-    { Public declarations }
+  TPos = record
+    x, y: real;
+    constructor Create(x, y: real);
+    procedure Interpolate(const First, Second: TPos; t: real);
+    class operator Add(const Lhs, Rhs: TPos): TPos;
   end;
 
-type
-  TScene = array of TAnimObject;
+  TTransform = record
+    pos: TPos;
+    rotation: real;
+    scale: real;
 
-var
-  Form1: TForm1;
-  pos1, pos2: THuman;
-  sHuman: Thuman;
-  sHuman1,sHuman2,sHuman3,sHuman4: Thuman;
+    constructor Create(const pos: TPos; const rotation, scale: real); overload;
+    constructor Create(const pos: TPos); overload;
+    procedure Interpolate(const First, Second: TTransform; t: real);
+    function ScaleRotate(const Point: TPos): TPos;
+  end;
 
-  Circle: TCircle;
+  TAnimObject = class
+    Transform: TTransform;
+    procedure Interpolate(const First, Second: TAnimObject; t: real);
+    procedure Draw(Canvas: TCanvas); virtual;
+  end;
 
-  drevo: TDrevo;
-  House: THouse;
-  door: TDoor;
-  hill: THill;
-  Stars:TStars;
-  Moon : TMoon;
+  TCircle = class(TAnimObject)
+    radius: real;
+    constructor Create(x, y, radius: real);
+    procedure Interpolate(const First, Second: TCircle; t: real);
+    procedure Draw(Canvas: TCanvas); override;
+  end;
 
-  time, dSpeed: Real;
-  dClose:boolean;
-  force: Real;
-  State, Pos:Real;
-  scene: TScene;
+  TLimb = class
+    angle: real;
+    len: real;
+    dPos: TPos;
+    aPos: TPos;
+    procedure CalcDPos;
+    procedure SetAngle(Iangle: real);
+    procedure Interpolate(const First, Second: TLimb; t: real);
+    constructor Create(Iangle: real; Ilen: real);
+  end;
+
+  THuman = class(TAnimObject)
+  private
+    Limbs: array [0 .. 9] of TLimb;
+
+  public
+    head_size: real;
+
+    property LForearm: TLimb read Limbs[0] write Limbs[0];
+    property RForearm: TLimb read Limbs[1] write Limbs[1];
+    property LUpperarm: TLimb read Limbs[2] write Limbs[2];
+    property RUpperarm: TLimb read Limbs[3] write Limbs[3];
+    property LThigh: TLimb read Limbs[4] write Limbs[4];
+    property RThigh: TLimb read Limbs[5] write Limbs[5];
+    property LShin: TLimb read Limbs[6] write Limbs[6];
+    property RShin: TLimb read Limbs[7] write Limbs[7];
+    property Torso: TLimb read Limbs[8] write Limbs[8];
+    property Neck: TLimb read Limbs[9] write Limbs[9];
+
+    procedure Draw(Canvas: TCanvas); override;
+    procedure Update;
+    procedure Interpolate(const First, Second: THuman; t: real);
+    Constructor DefaultTPose;
+  end;
+
+procedure Circle(Canvas: TCanvas; x, y, r: real);
+function Deg2Rad(deg: real): real;
 
 implementation
 
-{$R *.dfm}
-
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TAnimObject.Draw(Canvas: TCanvas);
 begin
-  sHuman := THuman.DefaultTPose;
-  sHuman.Update;
+  //Canvas.MoveTo(0, 0);
+  //Canvas.LineTo(100, 100);
+end;
 
-  sHuman1 := THuman.DefaultTPose;
-  with sHuman1 do
-  begin
-  Torso.SetAngle(deg2rad(-80));
+procedure Circle(Canvas: TCanvas; x, y, r: real);
+begin
+  Canvas.Ellipse(round(x - r), round(y - r), round(x + r), round(y + r));
+end;
 
-  LUpperarm.SetAngle(Deg2Rad(150));
-  LForearm.SetAngle(Deg2Rad(70));
+function Deg2Rad(deg: real): real;
+begin
+  result := deg * Pi / 180.0;
+end;
 
-  RUpperarm.SetAngle(Deg2Rad(80));
-  RForearm.SetAngle(Deg2Rad(-10));
+// Linear interpolation
+function lerp(v0, v1, t: real): real;
+begin
+  result := v0 + t * (v1 - v0);
+end;
 
-  RThigh.SetAngle(Deg2Rad(40));
-  RShin.SetAngle(Deg2Rad(100));
+procedure RotateScale(var x, y: real; scale, angle: real);
+var
+  nx, ny: real;
+  s, c: real;
+begin
+  s := sin(angle);
+  c := cos(angle);
 
-  LThigh.SetAngle(Deg2Rad(120));
-  LShin.SetAngle(Deg2Rad(130));
-  end;
+  nx := x * c - y * s;
+  ny := x * s + y * c;
+  x := nx * scale;
+  y := ny * scale;
+end;
 
-  sHuman2 := THuman.DefaultTPose;
-  with sHuman2 do
-  begin
-  LUpperarm.SetAngle(Deg2Rad(80));
-  LForearm.SetAngle(Deg2Rad(30));
+constructor TPos.Create(x: real; y: real);
+begin
+  Self.x := x;
+  Self.y := y;
+end;
 
-  RUpperarm.SetAngle(Deg2Rad(100));
-  RForearm.SetAngle(Deg2Rad(70));
+class operator TPos.Add(const Lhs, Rhs: TPos): TPos;
+begin
+  result.x := Lhs.x + Rhs.x;
+  result.y := Lhs.y + Rhs.y;
+end;
 
-  RThigh.SetAngle(Deg2Rad(80));
-  RShin.SetAngle(Deg2Rad(95));
+procedure TPos.Interpolate(const First, Second: TPos; t: real);
+begin
+  Self.x := lerp(First.x, Second.x, t);
+  Self.y := lerp(First.y, Second.y, t);
+end;
 
-  LThigh.SetAngle(Deg2Rad(50));
-  LShin.SetAngle(Deg2Rad(150));
-  end;
+procedure TAnimObject.Interpolate(const First, Second: TAnimObject; t: real);
+begin
+  Transform.Interpolate(First.Transform, Second.Transform, t);
+end;
 
-  sHuman3 := THuman.DefaultTPose;
-  with sHuman3 do
-  begin
-  Torso.SetAngle(Deg2Rad(-80));
-  RUpperarm.SetAngle(Deg2Rad(150));
-  RForearm.SetAngle(Deg2Rad(70));
+constructor TTransform.Create(const pos: TPos; const rotation, scale: real);
+begin
+  Self.pos := pos;
+  Self.rotation := rotation;
+  Self.scale := scale;
+end;
 
-  LUpperarm.SetAngle(Deg2Rad(80));
-  LForearm.SetAngle(Deg2Rad(-10));
+constructor TTransform.Create(const pos: TPos);
+begin
+  Self.pos := pos;
+  Self.rotation := 0;
+  Self.scale := 1;
+end;
 
-  LThigh.SetAngle(Deg2Rad(40));
-  LShin.SetAngle(Deg2Rad(100));
+procedure TTransform.Interpolate(const First, Second: TTransform; t: real);
+begin
+  pos.Interpolate(First.pos, Second.pos, t);
+  scale := lerp(First.scale, Second.scale, t);
+  rotation := lerp(First.rotation, Second.rotation, t);
+end;
 
-  RThigh.SetAngle(Deg2Rad(120));
-  RShin.SetAngle(Deg2Rad(130));
-  end;
-  {
-    Torso := TLimb.Create(Deg2Rad(-90), cTORSO_LEN);
+function TTransform.ScaleRotate(const Point: TPos): TPos;
+var
+  nx, ny: real;
+  s, c: real;
+begin
+  s := sin(rotation);
+  c := cos(rotation);
+
+  result.x := (Point.x * c - Point.y * s) * scale;
+  result.y := (Point.x * s + Point.y * c) * scale;
+end;
+
+constructor TCircle.Create(x, y, radius: real);
+begin
+  Self.Transform := TTransform.Create(TPos.Create(x, y), 0, 1);
+  Self.radius := radius;
+end;
+
+procedure TCircle.Draw(Canvas: TCanvas);
+var
+  r: real;
+begin
+  r := radius * Transform.scale;
+  with Transform.pos do
+    Canvas.Ellipse(round(x - r), round(y - r), round(x + r), round(y + r));
+end;
+
+procedure TCircle.Interpolate(const First: TCircle;
+  const Second: TCircle; t: real);
+begin
+  inherited Interpolate(First, Second, t);
+  radius := lerp(First.radius, Second.radius, t);
+end;
+
+constructor TLimb.Create(Iangle: real; Ilen: real);
+begin
+  angle := Iangle;
+  len := Ilen;
+  CalcDPos;
+end;
+
+procedure TLimb.CalcDPos;
+begin
+  dPos.x := len * cos(angle);
+  dPos.y := len * sin(angle);
+end;
+
+procedure TLimb.SetAngle(Iangle: real);
+begin
+  angle := Iangle;
+  CalcDPos;
+end;
+
+procedure TLimb.Interpolate(const First: TLimb; const Second: TLimb; t: real);
+begin
+  Self.len := lerp(First.len, Second.len, t);
+  SetAngle(lerp(First.angle, Second.angle, t));
+end;
+
+constructor THuman.DefaultTPose;
+const
+  DEFAULT_X = 200;
+  DEFAULT_Y = 400;
+
+  cHEAD_SIZE = 10;
+
+  cNECK_LEN = 20;
+  cTORSO_LEN =60;
+  cARMS_LEN = 30;
+  cLEGS_LEN = 30;
+
+
+  angle_UP = -90;
+
+begin
+  Transform := TTransform.Create(TPos.Create(DEFAULT_X, DEFAULT_Y));
+  head_size := cHEAD_SIZE;
+
+  Torso := TLimb.Create(Deg2Rad(-90), cTORSO_LEN);
   Neck := TLimb.Create(Deg2Rad(-90), cNECK_LEN);
-
   LUpperarm := TLimb.Create(Deg2Rad(120), cARMS_LEN);
-  RUpperarm := TLimb.Create(Deg2Rad(60), cARMS_LEN);
-  LForearm := TLimb.Create(Deg2Rad(90), cARMS_LEN);
-  RForearm := TLimb.Create(Deg2Rad(90), cARMS_LEN);
-
-  LThigh := TLimb.Create(Deg2Rad(70), cLEGS_LEN);
+	@@ -251,76 +117,138 @@ constructor THuman.DefaultTPose;
   LShin := TLimb.Create(Deg2Rad(90), cLEGS_LEN);
   RThigh := TLimb.Create(Deg2Rad(110), cLEGS_LEN);
   RShin := TLimb.Create(Deg2Rad(90), cLEGS_LEN);
-
-   h3.Torso.SetAngle(Deg2Rad(-80));
-  h3.RUpperarm.SetAngle(Deg2Rad(150));
-  h3.RForearm.SetAngle(Deg2Rad(70));
-
-  h3.LUpperarm.SetAngle(Deg2Rad(80));
-  h3.LForearm.SetAngle(Deg2Rad(-10));
-
-  h3.LThigh.SetAngle(Deg2Rad(40));
-  h3.LShin.SetAngle(Deg2Rad(100));
-
-  h3.RThigh.SetAngle(Deg2Rad(120));
-  h3.RShin.SetAngle(Deg2Rad(130));
-  }
-  drevo := TDrevo.Create;
-  House := THouse.Create;
-  door := TDoor.Create;
-  hill := THill.Create;
-  Stars := TStars.Create;
-  Moon  := TMoon.Create;
-
-  SetLength(Scene, 7);
-  scene[0] := Stars;
-  scene[1] := Moon;
-  scene[2] := Drevo;
-  scene[3] := Hill;
-  scene[4] := SHuman;
-  scene[5] := House;
-  scene[6] := Door;
-
-
-  time := 0;
-  dSpeed:=deg2rad(1.5);
-  dClose:=false;
-  force := -2;
-
-  state:=0;
-  pos:=sHuman.Transform.pos.x;
 end;
-
-procedure TForm1.Timer1Timer(Sender: TObject);
-const
-  MAX_ANGLE = -135;
-begin
-  // Update
-  state:=state+0.1;
-  pos:=pos+4;
-  if state > 3 then
-    state := 0;
-  if door.angle <= 0 then
-    dClose:=true;
-  if not(dClose) then
-    door.angle := door.angle - dSpeed
-  else
-    begin
-
-
-      if pos > 450 then
-      begin
-         if door.angle<= deg2rad(44) then
-            door.angle := door.angle + dSpeed;
-      end;
-    end;
-
-    if state<1 then
-          sHuman.Interpolate(sHuman, sHuman1, state)
-      else
-      begin
-        if state < 2 then
-          sHuman.Interpolate(sHuman, sHuman2, state-1)
-        else
-          if state < 3 then
-            sHuman.Interpolate(sHuman, sHuman3, state-2);
-      end;
-
-    sHuman.Transform.pos.x:=Pos;
-    sHuman.Update;
-
-    if door.angle>=deg2rad(45) then
-    begin
-    if (hill.smoothlen > 0) then
-      hill.smoothlen := hill.smoothlen - 4;
-    with house.Transform.pos do
-    begin
-        x:=x-4;
-    end;
-    with Door do
-    begin
-        Transform.pos.x:=Transform.pos.x-4;
-        ShadowX:=ShadowX-4;
-    end;
-    end;
-
-    if (hill.smoothlen < 0) then
-    begin
-      with drevo.Transform.pos do
-      begin
-      x:=x-12;
-      y:=y-4.5;
-
-      end;
-
-    end;
-
-  Self.Repaint; // Call Render
-end;
-
-procedure TForm1.FormKeyPress(Sender: TObject; var Key: Char);
-begin
-Form1.Close;
-end;
-
-procedure TForm1.FormPaint(Sender: TObject);
+procedure THuman.Interpolate(const First, Second: THuman; t: real);
 var
   I: Integer;
 begin
-  {Stars.Draw(Canvas);
-  Moon.Draw(Canvas);
-  drevo.Draw(PaintBox1.Canvas);
-  hill.Draw(Canvas);
-  House.Draw(PaintBox1.Canvas);
-
-  door.Draw(PaintBox1.Canvas);}
-
-  //ShowMessage('FUCK YOU');
-  // Render
-  for I := Low(Scene) to High(Scene) do
+  inherited Interpolate(First, Second, t);
+  head_size := lerp(First.head_size, Second.head_size, t);
+  for I := Low(Limbs) to High(Limbs) do
+    Limbs[I].Interpolate(First.Limbs[I], Second.Limbs[I], t);
+end;
+procedure THuman.Update;
+var
+  tp: TPos;
+begin
+  with Transform do
   begin
-    Scene[I].Draw(Canvas);
+    Torso.aPos := Transform.pos + ScaleRotate(Torso.dPos);
+    Neck.aPos := Torso.aPos + ScaleRotate(Neck.dPos);
+    LUpperarm.aPos := Torso.aPos + ScaleRotate(LUpperarm.dPos);
+    RUpperarm.aPos := Torso.aPos + ScaleRotate(RUpperarm.dPos);
+
+    LForearm.aPos := LUpperarm.aPos + ScaleRotate(LForearm.dPos);
+    RForearm.aPos := RUpperarm.aPos + ScaleRotate(RForearm.dPos);
+
+    LThigh.aPos := Transform.pos + ScaleRotate(LThigh.dPos);
+    RThigh.aPos := Transform.pos + ScaleRotate(RThigh.dPos);
+
+    LShin.aPos := LThigh.aPos + ScaleRotate(LShin.dPos);
+    RShin.aPos := RThigh.aPos + ScaleRotate(RShin.dPos);
   end;
-
-
 end;
 
-end.
+procedure THuman.Draw(Canvas: TCanvas);
